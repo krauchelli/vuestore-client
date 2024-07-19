3bd77b789940debd08772d8bc668d446ce9fcbdf
Krauchelli <mekaku26shi@gmail.com>
2024-07-19 15:01:26 +0700

    chore: Add Navbar component for improved navigation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Product</router-link> |
-      <router-link :to="{ name: 'product-detail', params: {id: 123}}">Detail</router-link> | 
-      <router-link to="/cart">Cart</router-link> |
-    </nav>
+    <Navbar />
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar';
+
+export default {
+  components: {
+    Navbar
+  }
+
+}
+</script>
 
 <style>
 * {
