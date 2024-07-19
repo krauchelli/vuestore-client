1232c67ca93b84ab2cc896db28312df0954c6de1
Krauchelli <mekaku26shi@gmail.com>
2024-07-19 16:10:58 +0700

    feat: Add ProductItem component for displaying individual products

--- src/views/product/Index.vue
+++ src/views/product/Index.vue
@@ -3,18 +3,15 @@
   <div>
     <div id="page-wrap">
         <div class="grid-wrap">
-            <div
-                v-for="product in products"
-                :key="product.id"
-                class="product-item"
-            >
-                <img :src="product.imageUrl" alt="">
-                <h3 class="product-name">{{ product.name }}</h3>
-                <p class="product-price">Rp{{ product.price }}</p>
-                <router-link :to="{ name: 'product-detail', params: {id: product.id}}">
-                    <button>Detail</button>
-                </router-link> | 
-            </div>
+            <ProductItem 
+              v-for="product in products"
+              :key="product.id" 
+
+              :product="product"
+            /> <!-- menampilkan product item, menggunakan konsep component dan prop
+              'product' pada :produt adalah prop yang nanti akan di passing ke ProductItem.vue,
+              sedangkan nilai dari product adalah product yang di loop dari 'products' -->
+            -->
         </div>
     </div>
   </div>
@@ -22,7 +19,12 @@
 
 <script>
 import { products } from '../../data-seed' 
+import ProductItem from '../../components/ProductItem.vue'
+
 export default {
+    components: {
+        ProductItem
+    },
     data() {
         return {
             products
@@ -37,36 +39,5 @@ export default {
     flex-wrap: wrap;
     justify-content: space-between;
     margin-top: 16px;
-  }
-
-  .product-item {
-    align-items: center;
-    border-radius: 8px;
-    box-shadow: 0px 2px 5px #888;
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 2%;
-    padding: 20px;
-    position: relative;
-    width: 32%;
-  }
-
-  .product-name {
-    margin-bottom: 0;
-  }
-
-  img {
-    height: 200px;
-    width: 200px;
-    border-radius: 5%;
-  }
-
-  a {
-    width: 100%;
-  }
-
-  button {
-    width: 100%;
-  }
-
+  } 
 </style>
